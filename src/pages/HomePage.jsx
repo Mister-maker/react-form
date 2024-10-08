876ebf846c44d75809194fce885d0cd79f2d6b71
mister-maker <mkaggrawal97@gmail.com>
2024-10-08 11:38:26 +0530

    added lottie animation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { auth } from "../config/firebase";
+import Lottie from "lottie-react";
+import loaderAnimaiton from "../assets/animation.json";
 
 const HomePage = () => {
     const user = auth.currentUser;
-    return <div>{user?.displayName}</div>;
+    return (
+        <div>
+            <Lottie
+                animationData={loaderAnimaiton}
+                loop={true}
+                style={{ height: 200 }}
+            />
+            {user?.displayName}
+        </div>
+    );
 };
 
 export default HomePage;
